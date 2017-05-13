f93bfc80f12355a26f3be1bc01f71ef3881e9abb
Vadim Nesterenko <vadim.nesterenko@saltpepper.ru>
2017-05-13 16:16:28 +0300

    fix count

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -10,7 +10,7 @@ class Shift < ApplicationRecord
   after_commit :check_payments_count, on: [:create, :update]
 
   def check_payments_count
-  	(female_count+male_count-self.payments.where.not(is_coordinator: true, is_reserve: true)).times do
+  	(female_count+male_count-self.payments.where.not(is_coordinator: true, is_reserve: true).count).times do
   		self.payments.create
   	end
   end
